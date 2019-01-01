7c4643dbbae8aa32e8fd8b54ca57c7b19d40ecfe
SecyrBb <oleggedzjuns@gmail.com>
2019-01-01 13:49:33 +0200

    bugged version

---
+++ MultiThreadSort/Data/DataBase.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace MultiThreadSort.Data
+{
+    class DataBase
+    {
+        public int ListCnt;
+        public int ListSize;
+
+        public List<List<int>> Data { get; set; }
+
+        public DataBase(int listCnt, int listSize, int randMin, int randMax)
+        {
+            ListCnt = listCnt;
+            ListSize = listSize;
+            Data = new List<List<int>>(ListCnt);
+            for (int i = 0; i < ListCnt; i++)
+            {
+                Random rand = new Random();
+                List<int> buf = new List<int>(ListSize);
+                for(int j = 0; j < ListSize; j++)
+                {
+                    buf.Add(rand.Next(randMin, randMax));
+                }
+                Data.Add(buf);
+            }
+        }
+
+        public void SingleThreadSort()
+        {
+            for(int i = 0; i < ListCnt; i++)
+            {
+                Data[i].Sort();
+            }
+        }
+
+        public void MultiThreadSort()
+        {
+            Thread t1 = new Thread(Func);
+            Thread t2 = new Thread(Func);
+            Thread t3 = new Thread(Func);
+            for (int i = 0; i < ListCnt; i += 4)
+            {
+                t1.Start(Data[i]);
+                t2.Start(Data[i + 1]);
+                t3.Start(Data[i + 2]);
+                Data[i + 3].Sort();
+                t1.Join();
+                t2.Join();
+                t3.Join();
+            }
+        }
+        void Func(Object dat)
+        {
+            List<int> Dat = (List<int>)dat;
+            Dat.Sort();
+        }
+    }
+}
--- MultiThreadSort/Program.cs
+++ MultiThreadSort/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MultiThreadSort.Data;
 
 namespace MultiThreadSort
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var watch = Stopwatch.StartNew();
+            DataBase dat = new DataBase(1000, 1000, 0, 1000000);
+            //DataBase dat2 = new DataBase(10000, 10000, 0, 1000000);
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"Created in {elapsedMs} ms");
+
+            //watch.Restart();
+            //dat.SingleThreadSort();
+            //watch.Stop();
+            //elapsedMs = watch.ElapsedMilliseconds;
+            //Console.WriteLine($"Single thread sort time = {elapsedMs} ms");
+
+            watch.Restart();
+            dat.MultiThreadSort();
+            watch.Stop();
+            elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"Multi thread sort time = {elapsedMs} ms");
+
+            Console.ReadKey(false);
         }
     }
 }
